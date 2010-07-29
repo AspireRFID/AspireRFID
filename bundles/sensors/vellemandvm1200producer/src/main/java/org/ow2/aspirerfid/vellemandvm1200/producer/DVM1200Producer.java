17c9ec48f7d6c78a35754a43261018f02dda4b53
calmant <calmant@3fbdb089-5876-4d40-aeb2-f6ac8130dd8c>
2010-07-29 18:36:52 +0000

    Automatically add the port attribute in the MBean name

--- bundles/sensors/vellemandvm1200producer/src/main/java/org/ow2/aspirerfid/vellemandvm1200/producer/DVM1200Producer.java
+++ bundles/sensors/vellemandvm1200producer/src/main/java/org/ow2/aspirerfid/vellemandvm1200/producer/DVM1200Producer.java
@@ -35,6 +35,9 @@ import java.util.Hashtable;
 import java.util.Map;
 import java.util.TooManyListenersException;
 
+import javax.management.MBeanRegistration;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 import javax.management.openmbean.CompositeData;
 import javax.management.openmbean.CompositeDataSupport;
 import javax.management.openmbean.CompositeType;
@@ -52,14 +55,15 @@ import org.osgi.service.wireadmin.WireConstants;
 import org.osgi.util.measurement.Measurement;
 import org.ow2.aspirerfid.vellemandvm1200.data.DVM1200MultimeterAcquisition;
 
-public class DVM1200Producer implements Producer, SerialPortEventListener {
+public class DVM1200Producer implements Producer, SerialPortEventListener,
+		MBeanRegistration {
 
 	/** Default bits per second for COM port. */
 	private static final int DATA_RATE = 2400;
 
 	/** Milliseconds to block while waiting for port open */
 	private static final int TIME_OUT = 2000;
-	
+
 	private String m_PID;
 
 	private String m_portName;
@@ -67,29 +71,27 @@ public class DVM1200Producer implements Producer, SerialPortEventListener {
 	private Wire m_wires[];
 
 	private LogService m_logService;
-	
+
 	private Measurement m_lastMeasurement;
-	
+
 	// for JMX MBean
 	private Map m_lastMeasurementMap;
-	private double m_lastMeasurementValue=Double.NaN;
-	private double m_lastMeasurementError=Double.NaN;
-	private long m_lastMeasurementTime=0;
+	private double m_lastMeasurementValue = Double.NaN;
+	private double m_lastMeasurementError = Double.NaN;
+	private long m_lastMeasurementTime = 0;
 	private String m_lastMeasurementUnit = null;
 	private CompositeData m_lastMeasurementComposite;
-	
-	
+
 	private CompositeType m_lastMeasurementCompositeType;
 
-	private static final String[] m_itemsNames = {"time", "unit", "value", "error"};
-	
-	private static final String[] m_itemsDescr = {"T", "U", "V", "E"};
-	
-	private static final OpenType[] m_itemsTypes = {
-		SimpleType.LONG, SimpleType.STRING, SimpleType.DOUBLE, SimpleType.DOUBLE
-	};
-	
-	
+	private static final String[] m_itemsNames = { "time", "unit", "value",
+			"error" };
+
+	private static final String[] m_itemsDescr = { "T", "U", "V", "E" };
+
+	private static final OpenType[] m_itemsTypes = { SimpleType.LONG,
+			SimpleType.STRING, SimpleType.DOUBLE, SimpleType.DOUBLE };
+
 	private SerialPort serialPort = null;
 
 	private OutputStream outputStream = null;
@@ -99,12 +101,8 @@ public class DVM1200Producer implements Producer, SerialPortEventListener {
 	// TODO resizable buffer
 	byte[] readBuffer = new byte[256];
 
-	private static final Class[] m_flavors = new Class[] {
-			Measurement.class,
-			Double.class,
-			Float.class,
-			String.class
-			};
+	private static final Class[] m_flavors = new Class[] { Measurement.class,
+			Double.class, Float.class, String.class };
 
 	private BundleContext m_bundleContext;
 
@@ -115,9 +113,9 @@ public class DVM1200Producer implements Producer, SerialPortEventListener {
 
 	public DVM1200Producer(BundleContext bc) throws OpenDataException {
 		m_bundleContext = bc;
-		
-		m_lastMeasurementCompositeType = new CompositeType("DVM1200Measurement",
-				"Measurements from Velleman DVM1200",
+
+		m_lastMeasurementCompositeType = new CompositeType(
+				"DVM1200Measurement", "Measurements from Velleman DVM1200",
 				m_itemsNames, m_itemsDescr, m_itemsTypes);
 	}
 
@@ -125,7 +123,7 @@ public class DVM1200Producer implements Producer, SerialPortEventListener {
 
 		// open the port and register a listener
 		open();
-		
+
 		// registration of the Consumer service
 		Hashtable props = new Hashtable();
 		props.put(Constants.SERVICE_PID, m_PID);
@@ -172,50 +170,49 @@ public class DVM1200Producer implements Producer, SerialPortEventListener {
 		return null;
 	}
 
-	private void log(int level, String message){
-		if(m_logService!=null) {
+	private void log(int level, String message) {
+		if (m_logService != null) {
 			m_logService.log(level, message);
 		}
 	}
 
-	private void log(int level, String message, Throwable throwable){
-		if(m_logService!=null) {
-			m_logService.log(level, message,throwable);
+	private void log(int level, String message, Throwable throwable) {
+		if (m_logService != null) {
+			m_logService.log(level, message, throwable);
 		}
 	}
 
-	
 	private void open() {
 
 		CommPortIdentifier portId = null;
 		try {
 			portId = CommPortIdentifier.getPortIdentifier(m_portName);
 		} catch (NoSuchPortException e) {
-			log(LogService.LOG_ERROR,"No Such Port:" + m_portName,e);
+			log(LogService.LOG_ERROR, "No Such Port:" + m_portName, e);
 			return;
 		}
 		if (portId.getPortType() != CommPortIdentifier.PORT_SERIAL) {
-			log(LogService.LOG_ERROR,m_portName+ " is not a serial port");
+			log(LogService.LOG_ERROR, m_portName + " is not a serial port");
 			return;
 		}
 		try {
 			serialPort = (SerialPort) portId.open("VellemanDVM1200", TIME_OUT);
 		} catch (PortInUseException e) {
-			log(LogService.LOG_ERROR,m_portName + " already used",e);
+			log(LogService.LOG_ERROR, m_portName + " already used", e);
 			return;
 		}
 
 		try {
 			outputStream = serialPort.getOutputStream();
 		} catch (IOException e) {
-			log(LogService.LOG_ERROR,e.getMessage(),e);
+			log(LogService.LOG_ERROR, e.getMessage(), e);
 			return;
 		}
 
 		try {
 			inputStream = serialPort.getInputStream();
 		} catch (IOException e) {
-			log(LogService.LOG_ERROR,e.getMessage(),e);
+			log(LogService.LOG_ERROR, e.getMessage(), e);
 			return;
 		}
 
@@ -223,20 +220,16 @@ public class DVM1200Producer implements Producer, SerialPortEventListener {
 			serialPort.addEventListener(this);
 			serialPort.notifyOnDataAvailable(true);
 		} catch (TooManyListenersException e) {
-			log(LogService.LOG_ERROR,e.getMessage(),e);
+			log(LogService.LOG_ERROR, e.getMessage(), e);
 			return;
 		}
 
 		try {
 			// set port parameters
-			serialPort.setSerialPortParams(
-					DATA_RATE,
-					SerialPort.DATABITS_8,
-					SerialPort.STOPBITS_1,
-					SerialPort.PARITY_NONE
-			);
+			serialPort.setSerialPortParams(DATA_RATE, SerialPort.DATABITS_8,
+					SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
 		} catch (UnsupportedCommOperationException e) {
-			log(LogService.LOG_ERROR,e.getMessage(),e);
+			log(LogService.LOG_ERROR, e.getMessage(), e);
 			return;
 		}
 
@@ -244,14 +237,14 @@ public class DVM1200Producer implements Producer, SerialPortEventListener {
 
 	private void close() {
 		if (serialPort == null) {
-			log(LogService.LOG_ERROR,"No opened port");
+			log(LogService.LOG_ERROR, "No opened port");
 			return;
 		}
 		if (outputStream != null) {
 			try {
 				outputStream.close();
 			} catch (IOException e) {
-				log(LogService.LOG_ERROR,e.getMessage(),e);
+				log(LogService.LOG_ERROR, e.getMessage(), e);
 			}
 			outputStream = null;
 		}
@@ -260,7 +253,7 @@ public class DVM1200Producer implements Producer, SerialPortEventListener {
 			try {
 				inputStream.close();
 			} catch (IOException e) {
-				log(LogService.LOG_ERROR,e.getMessage(),e);
+				log(LogService.LOG_ERROR, e.getMessage(), e);
 			}
 			inputStream = null;
 		}
@@ -298,54 +291,72 @@ public class DVM1200Producer implements Producer, SerialPortEventListener {
 						numBytes = inputStream.read(readBuffer);
 
 						try {
-							DVM1200MultimeterAcquisition acquisition = new DVM1200MultimeterAcquisition(readBuffer);
-							
-							if(acquisition.isOverRange() || acquisition.isDataHoldMode() || acquisition.isMin() || acquisition.isMax())
+							DVM1200MultimeterAcquisition acquisition = new DVM1200MultimeterAcquisition(
+									readBuffer);
+
+							if (acquisition.isOverRange()
+									|| acquisition.isDataHoldMode()
+									|| acquisition.isMin()
+									|| acquisition.isMax())
 								continue;
-							
-							String unitStr=acquisition.getUnit();
-							Measurement m = new Measurement(UnitUtil.convertValue(acquisition.getValue(),unitStr), UnitUtil.convertError(acquisition.getError(),unitStr) , UnitUtil.getUnit(unitStr),System.currentTimeMillis());
-							
+
+							String unitStr = acquisition.getUnit();
+							Measurement m = new Measurement(
+									UnitUtil.convertValue(
+											acquisition.getValue(), unitStr),
+									UnitUtil.convertError(
+											acquisition.getError(), unitStr),
+									UnitUtil.getUnit(unitStr),
+									System.currentTimeMillis());
+
 							// set the last value for the wires
 							m_lastMeasurement = m;
-							
+
 							// set the last value for the JMX attribute
-							Map map=new HashMap();
-							map.put("value", new Double(m_lastMeasurement.getValue()));
-							map.put("error", new Double(m_lastMeasurement.getError()));
-							map.put("unit", UnitUtil.toString(m_lastMeasurement.getUnit()));
-							map.put("time", new Long(m_lastMeasurement.getTime()));
+							Map map = new HashMap();
+							map.put("value",
+									new Double(m_lastMeasurement.getValue()));
+							map.put("error",
+									new Double(m_lastMeasurement.getError()));
+							map.put("unit", UnitUtil.toString(m_lastMeasurement
+									.getUnit()));
+							map.put("time",
+									new Long(m_lastMeasurement.getTime()));
 							m_lastMeasurementMap = map; // injected by iPOJO
-							
+
 							try {
-								CompositeData tempData = new CompositeDataSupport(m_lastMeasurementCompositeType, map);
+								CompositeData tempData = new CompositeDataSupport(
+										m_lastMeasurementCompositeType, map);
 								m_lastMeasurementComposite = tempData;
 							} catch (OpenDataException e) {
 								// TODO Auto-generated catch block
 								e.printStackTrace();
 							}
-							
-														
-							if(m_lastMeasurement.getValue()!=m_lastMeasurementValue) {
-								m_lastMeasurementValue=m_lastMeasurement.getValue();
+
+							if (m_lastMeasurement.getValue() != m_lastMeasurementValue) {
+								m_lastMeasurementValue = m_lastMeasurement
+										.getValue();
 							}
-							
-							if(m_lastMeasurement.getError()!=m_lastMeasurementError) {
-								m_lastMeasurementError=m_lastMeasurement.getError();
+
+							if (m_lastMeasurement.getError() != m_lastMeasurementError) {
+								m_lastMeasurementError = m_lastMeasurement
+										.getError();
 							}
-							
+
 							String curUnit;
-							if(!(curUnit = UnitUtil.toString(m_lastMeasurement.getUnit())).equals(m_lastMeasurementUnit)) {
-								m_lastMeasurementUnit=curUnit;
+							if (!(curUnit = UnitUtil.toString(m_lastMeasurement
+									.getUnit())).equals(m_lastMeasurementUnit)) {
+								m_lastMeasurementUnit = curUnit;
 							}
-							
-							if(m_lastMeasurement.getTime()!=m_lastMeasurementTime) {
-								m_lastMeasurementTime=m_lastMeasurement.getTime();
+
+							if (m_lastMeasurement.getTime() != m_lastMeasurementTime) {
+								m_lastMeasurementTime = m_lastMeasurement
+										.getTime();
 							}
-							
+
 							// System.out.println(acquisition.toString()+"-->"+m.toString());
-							log(LogService.LOG_INFO,"Acquire "+m.toString());
-							
+							log(LogService.LOG_INFO, "Acquire " + m.toString());
+
 							synchronized (this) {
 								for (int i = 0; m_wires != null
 										&& i < m_wires.length; i++) {
@@ -356,15 +367,54 @@ public class DVM1200Producer implements Producer, SerialPortEventListener {
 								}
 							}
 						} catch (ParseException e) {
-							log(LogService.LOG_ERROR,e.getMessage(),e);
+							log(LogService.LOG_ERROR, e.getMessage(), e);
 							return;
 						}
 					}
 				}
 			} catch (IOException e) {
-				log(LogService.LOG_ERROR,e.getMessage(),e);
+				log(LogService.LOG_ERROR, e.getMessage(), e);
 			}
 			break;
 		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.management.MBeanRegistration#preRegister(javax.management.MBeanServer
+	 * , javax.management.ObjectName)
+	 */
+	public ObjectName preRegister(MBeanServer server, ObjectName name)
+			throws Exception {
+		// Add the device port to the MBean name
+		ObjectName newName = new ObjectName(name.getCanonicalName() + ",port="
+				+ m_portName);
+		return newName;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.management.MBeanRegistration#postDeregister()
+	 */
+	public void postDeregister() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.management.MBeanRegistration#postRegister(java.lang.Boolean)
+	 */
+	public void postRegister(Boolean arg0) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.management.MBeanRegistration#preDeregister()
+	 */
+	public void preDeregister() throws Exception {
 	}
 }
