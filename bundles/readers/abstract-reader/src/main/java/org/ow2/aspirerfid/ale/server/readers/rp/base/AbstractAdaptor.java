1f06dcd69d74b187a631e59ad75783694b378290
gabo_pedraza <gabo_pedraza@3fbdb089-5876-4d40-aeb2-f6ac8130dd8c>
2010-09-21 12:01:14 +0000

    Base Driver Format

--- bundles/readers/abstract-reader/src/main/java/org/ow2/aspirerfid/ale/server/readers/rp/base/AbstractAdaptor.java
+++ bundles/readers/abstract-reader/src/main/java/org/ow2/aspirerfid/ale/server/readers/rp/base/AbstractAdaptor.java
@@ -20,7 +20,6 @@
 
    LGPL version 2.1 full text http://www.gnu.org/licenses/lgpl-2.1.txt    
  */
-
 package org.ow2.aspirerfid.ale.server.readers.rp.base;
 
 import java.net.MalformedURLException;
@@ -44,299 +43,286 @@ import org.ow2.aspirerfid.ale.server.readers.IdentifyThread;
  */
 public abstract class AbstractAdaptor extends BaseReader {
 
-	/** default readTimeInterval. */
-	public static final int DEFAULT_READTIME_INTERVAL = 2000;
+   /** default readTimeInterval. */
+   public static final int DEFAULT_READTIME_INTERVAL = 2000;
 
-	/** logger. */
-	private static final Logger LOG = Logger.getLogger(AbstractAdaptor.class);
+   /** logger. */
+   private static final Logger LOG = Logger.getLogger(AbstractAdaptor.class);
 
-	/** to get all the tags we need a polling thread. */
-	private IdentifyThread identifyThread = null;
+   /** to get all the tags we need a polling thread. */
+   private IdentifyThread identifyThread = null;
 
-	/** the interval in which shall be read from the reader. */
-	private int readTimeInterval = -1;
+   /** the interval in which shall be read from the reader. */
+   private int readTimeInterval = -1;
 
-	/** the reader */
-	private AbstractReader reader;
+   /** the reader */
+   private AbstractReader reader;
 
-	public AbstractAdaptor() {
-	   super();
-		LOG.info("Simple adaptor instantiated!!!");
+   public AbstractAdaptor() {
+      super();
+      LOG.info("Simple adaptor instantiated!!!");
    }
-	
 
-	/* (non-Javadoc)
-	 * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#initialize(java.lang.String, org.accada.ale.xsd.ale.epcglobal.LRSpec)
-	 */
-	@Override
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#initialize(java.lang.String,
+    * org.accada.ale.xsd.ale.epcglobal.LRSpec)
+    */
+   @Override
    public void initialize(String name, LRSpec spec) throws ImplementationExceptionResponse {
-	   super.initialize(name, spec);
-		try {
-			super.initialize(name, spec);
-		} catch (ImplementationExceptionResponse ie) {
-			LOG.error("error in initialize of superclass");
-			throw ie;
-		}
+      super.initialize(name, spec);
+      try {
+         super.initialize(name, spec);
+      } catch (ImplementationExceptionResponse ie) {
+         LOG.error("error in initialize of superclass");
+         throw ie;
+      }
 
-		try {
-			// extract from LRSpec how to connect to the reader
-			extractConnectionSettings();
-			 
-			reader = getReader();
-		} catch (ImplementationExceptionResponse ie) {
-			ie.printStackTrace();
-			LOG.error("could not extract connection settings from LRSpec");
-			throw new ImplementationExceptionResponse();
-		}
+      try {
+         // extract from LRSpec how to connect to the reader
+         extractConnectionSettings();
 
-		// connect to the reader
-		connectReader();	   
+         reader = getReader();
+      } catch (ImplementationExceptionResponse ie) {
+         ie.printStackTrace();
+         LOG.error("could not extract connection settings from LRSpec");
+         throw new ImplementationExceptionResponse();
+      }
+
+      // connect to the reader
+      connectReader();
    }
-	
-	/**
-	 * Gets a reference for the driver of actual reader
-	 * @return reader reference
-	 */
-	protected abstract AbstractReader getReader();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.ow2.aspirerfid.ale.server.readers.BaseReader#addTag(org.ow2.aspirerfid
-	 * .ale.server.Tag)
-	 */
-	@Override
-	public void addTag(Tag tag) {
-		setChanged();
-		tag.addTrace(getName());
-		notifyObservers(tag);
-	}
+   /**
+    * Gets a reference for the driver of actual reader
+    * 
+    * @return reader reference
+    */
+   protected abstract AbstractReader getReader();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.ow2.aspirerfid.ale.server.readers.BaseReader#addTags(java.util.List)
-	 */
-	@Override
-	public void addTags(List<Tag> tags) {
-		setChanged();
-		for (Tag tag : tags) {
-			tag.addTrace(getName());
-		}
-		notifyObservers(tags);
-	}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#addTag(org.ow2.aspirerfid .ale.server.Tag)
+    */
+   @Override
+   public void addTag(Tag tag) {
+      setChanged();
+      tag.addTrace(getName());
+      notifyObservers(tag);
+   }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#connectReader()
-	 */
-	@Override
-	public void connectReader() throws ImplementationExceptionResponse {
-		if (isConnected()) {
-			return;
-		}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#addTags(java.util.List)
+    */
+   @Override
+   public void addTags(List<Tag> tags) {
+      setChanged();
+      for (Tag tag : tags) {
+         tag.addTrace(getName());
+      }
+      notifyObservers(tags);
+   }
 
-		try {
-			startReader();
-		} catch (Exception e) {
-			setDisconnected();
-			throw new ImplementationExceptionResponse(e.getMessage());
-		}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#connectReader()
+    */
+   @Override
+   public void connectReader() throws ImplementationExceptionResponse {
+      if (isConnected()) {
+         return;
+      }
 
-		LOG.debug("setup identifyThread on RPAdaptor " + getName());
-		// setup the polling thread
-		identifyThread = new IdentifyThread(this);
-		identifyThread.setPollingFrequency(readTimeInterval);
-		identifyThread.start();
+      try {
+         startReader();
+      } catch (Exception e) {
+         setDisconnected();
+         throw new ImplementationExceptionResponse(e.getMessage());
+      }
 
-		// suspend the polling thread to the beginning
-		identifyThread.suspendIdentify();
+      LOG.debug("setup identifyThread on RPAdaptor " + getName());
+      // setup the polling thread
+      identifyThread = new IdentifyThread(this);
+      identifyThread.setPollingFrequency(readTimeInterval);
+      identifyThread.start();
 
-		setConnected();
+      // suspend the polling thread to the beginning
+      identifyThread.suspendIdentify();
 
-	}
+      setConnected();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#disconnectReader()
-	 */
-	@Override
-	public void disconnectReader() throws ImplementationExceptionResponse {
-		stopReader();
-		setReadTimeInterval(-1);
+   }
 
-		if (identifyThread != null) {
-			identifyThread.stopIdentify();
-			identifyThread = null;
-		}
-		setDisconnected();
-		setStopped();
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#disconnectReader()
+    */
+   @Override
+   public void disconnectReader() throws ImplementationExceptionResponse {
+      System.out.println("////////////// **************** The reader has been desactived //////////////// **********");
+      stopReader();
+      setReadTimeInterval(-1);
 
-	}
+      if (identifyThread != null) {
+         identifyThread.stopIdentify();
+         identifyThread = null;
+      }
+      setDisconnected();
+      setStopped();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.ow2.aspirerfid.ale.server.readers.BaseReader#identify(java.lang.String
-	 * [])
-	 */
-	@Override
-	public Observation[] identify(String[] readPointNames) throws HardwareException {
-		LOG.debug("identify called an RPAdaptor " + getName());
+   }
 
-		if ((countObservers() > 0)) {
-			reader.poll();
-			LOG.debug("Polling the reader");
-		} else {
-			LOG.debug("rp-proxy not ready (yet)");
-		}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#identify(java.lang.String [])
+    */
+   @Override
+   public Observation[] identify(String[] readPointNames) throws HardwareException {
+      LOG.debug("identify called an RPAdaptor " + getName());
 
-		return null;
-	}
+      if ((countObservers() > 0)) {
+         reader.poll();
+         LOG.debug("Polling the reader");
+      } else {
+         LOG.debug("rp-proxy not ready (yet)");
+      }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#start()
-	 */
-	@Override
-	public void start() {
-		if (!isConnected()) {
-			try {
-				connectReader();
-			} catch (ImplementationExceptionResponse e) {
-				e.printStackTrace();
-				setDisconnected();
-			}
-		}
+      return null;
+   }
 
-		// reader is not connected so it is not started as well
-		if (!isConnected()) {
-			setStopped();
-		} else {
-			LOG.debug("identifyThread starting to identify");
-			identifyThread.resumeIdentify();
-			setStarted();
-		}
-	}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#start()
+    */
+   @Override
+   public void start() {
+      if (!isConnected()) {
+         try {
+            connectReader();
+         } catch (ImplementationExceptionResponse e) {
+            e.printStackTrace();
+            setDisconnected();
+         }
+      }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#stop()
-	 */
-	@Override
-	public void stop() {
-		stopReader();
-		LOG.debug("identifyThread suspend to identify");
-		identifyThread.suspendIdentify();
-		setStopped();
-	}
+      // reader is not connected so it is not started as well
+      if (!isConnected()) {
+         setStopped();
+      } else {
+         LOG.debug("identifyThread starting to identify");
+         identifyThread.resumeIdentify();
+         setStarted();
+      }
+   }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.ow2.aspirerfid.ale.server.readers.BaseReader#update(org.accada.ale
-	 * .xsd.ale.epcglobal.LRSpec)
-	 */
-	@Override
-	public void update(LRSpec spec) throws ImplementationExceptionResponse {
-		stop();
-		setLRSpec(spec);
-		if (extractConnectionSettings()) {
-			LOG.debug("restarting reader " + getName());
-			disconnectReader();
-			// set the connection settings again and then start the reader
-			extractConnectionSettings();
-			connectReader();
-		}
-		start();
-	}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#stop()
+    */
+   @Override
+   public void stop() {
+      stopReader();
+      LOG.debug("identifyThread suspend to identify");
+      identifyThread.suspendIdentify();
+      setStopped();
+   }
 
-	
-	private void startReader() {
-		if (reader!=null)
-			reader.startReader();
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.ow2.aspirerfid.ale.server.readers.BaseReader#update(org.accada.ale .xsd.ale.epcglobal.LRSpec)
+    */
+   @Override
+   public void update(LRSpec spec) throws ImplementationExceptionResponse {
+      stop();
+      setLRSpec(spec);
+      if (extractConnectionSettings()) {
+         LOG.debug("restarting reader " + getName());
+         disconnectReader();
+         // set the connection settings again and then start the reader
+         extractConnectionSettings();
+         connectReader();
+      }
+      start();
+   }
 
-	}
+   private void startReader() {
+      if (reader != null)
+         reader.startReader();
 
-	private void stopReader() {
-		if (reader!=null)
-			reader.stopReader();
-	}
+   }
 
-	public int getReadTimeInterval() {
-		return readTimeInterval;
-	}
+   private void stopReader() {
+      if (reader != null)
+         reader.stopReader();
+   }
 
-	public void setReadTimeInterval(int readTimeInterval) {
-		this.readTimeInterval = readTimeInterval;
-	}
+   public int getReadTimeInterval() {
+      return readTimeInterval;
+   }
 
-	/**
-	 * this method extracts the connection settings from the LRProperty. when
-	 * there is the need to disconnect the reader (when something in the
-	 * connection to the reader has been changed in the LRSpec), true will be
-	 * returned false otherwise. if necessary the reader will be restarted
-	 * 
-	 * @throws ImplementationException
-	 *            whenever an error occurs
-	 * @return returns true if the connection to the reader needs to be
-	 *         reconnected
-	 */
-	private boolean extractConnectionSettings() throws ImplementationExceptionResponse {
+   public void setReadTimeInterval(int readTimeInterval) {
+      this.readTimeInterval = readTimeInterval;
+   }
 
-		// URL connectionPoint = toURL(logicalReaderProperties.get("ConnectionPoint"), "ConnectionPoint");
-		// URL notificationPoint = toURL(logicalReaderProperties.get("NotificationPoint"), "NotificationPoint");
-		String interval = logicalReaderProperties.get("ReadTimeInterval");
+   /**
+    * this method extracts the connection settings from the LRProperty. when there is the need to disconnect the reader
+    * (when something in the connection to the reader has been changed in the LRSpec), true will be returned false
+    * otherwise. if necessary the reader will be restarted
+    * 
+    * @throws ImplementationException whenever an error occurs
+    * @return returns true if the connection to the reader needs to be reconnected
+    */
+   private boolean extractConnectionSettings() throws ImplementationExceptionResponse {
 
-		setReadTimeInterval(-1);
+      // URL connectionPoint = toURL(logicalReaderProperties.get("ConnectionPoint"), "ConnectionPoint");
+      // URL notificationPoint = toURL(logicalReaderProperties.get("NotificationPoint"), "NotificationPoint");
+      String interval = logicalReaderProperties.get("ReadTimeInterval");
 
-		try {
-			setReadTimeInterval(Integer.parseInt(interval));
-		} catch (Exception ne) {
-			LOG.error("could not extract readTimeIntervall from LRPropery");
-			throw new ImplementationExceptionResponse("Could not extract ReadTimeInterval from LRPropery");
-		}
+      setReadTimeInterval(-1);
 
-		// assert that the readTimeInterval is not -1
-		if (readTimeInterval == -1) {
-			LOG.error("ReadTimeInterval not set - assuming 2000ms");
-			setReadTimeInterval(DEFAULT_READTIME_INTERVAL);
-		}
+      try {
+         setReadTimeInterval(Integer.parseInt(interval));
+      } catch (Exception ne) {
+         LOG.error("could not extract readTimeIntervall from LRPropery");
+         throw new ImplementationExceptionResponse("Could not extract ReadTimeInterval from LRPropery");
+      }
 
-		return false;
-	}
+      // assert that the readTimeInterval is not -1
+      if (readTimeInterval == -1) {
+         LOG.error("ReadTimeInterval not set - assuming 2000ms");
+         setReadTimeInterval(DEFAULT_READTIME_INTERVAL);
+      }
 
-	/**
-	 * this method calls the URL constructor with a string.
-	 * 
-	 * @param urlString
-	 *           the URL to be converted into a java.net.URL
-	 * @param comment
-	 *           "NotificationPoint" or " ConnectionPoint"
-	 * @return a URL created from urlString
-	 * @throws ImplementationException
-	 *            when a MalformedURLException is thrown
-	 */
-	private URL toURL(String urlString, String comment) throws ImplementationExceptionResponse {
-		URL url = null;
-		try {
-			url = new URL(urlString);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-			throw new ImplementationExceptionResponse("Could not extract " + comment + " from LRProperty");
-		}
-		return url;
-	}
+      return false;
+   }
 
-
+   /**
+    * this method calls the URL constructor with a string.
+    * 
+    * @param urlString the URL to be converted into a java.net.URL
+    * @param comment "NotificationPoint" or " ConnectionPoint"
+    * @return a URL created from urlString
+    * @throws ImplementationException when a MalformedURLException is thrown
+    */
+   private URL toURL(String urlString, String comment) throws ImplementationExceptionResponse {
+      URL url = null;
+      try {
+         url = new URL(urlString);
+      } catch (MalformedURLException e) {
+         e.printStackTrace();
+         throw new ImplementationExceptionResponse("Could not extract " + comment + " from LRProperty");
+      }
+      return url;
+   }
 
 }
-
