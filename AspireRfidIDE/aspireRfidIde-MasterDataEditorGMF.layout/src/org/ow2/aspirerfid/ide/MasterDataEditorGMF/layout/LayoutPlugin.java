54fee9ad2a2b380cbc3fc5b968d98c325e0fdf8f
elka <elka@3fbdb089-5876-4d40-aeb2-f6ac8130dd8c>
2011-05-19 20:04:29 +0000

    fixed issue with circular dependencies

---
+++ AspireRfidIDE/aspireRfidIde-MasterDataEditorGMF.layout/src/org/ow2/aspirerfid/ide/MasterDataEditorGMF/layout/LayoutPlugin.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright © 2008-2010, Aspire
+ * 
+ * Aspire is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License version 2.1 as published by
+ * the Free Software Foundation (the "LGPL").
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library in the file COPYING-LGPL-2.1; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ * 
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied. See the GNU Lesser General Public License
+ * for the specific language governing rights and limitations.
+ */
+ 
+package org.ow2.aspirerfid.ide.MasterDataEditorGMF.layout;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * @author Eleftherios Karageorgiou (elka) e-mail: elka@ait.edu.gr
+ *  
+ * The main plugin class to be used in the desktop.
+ */
+public class LayoutPlugin
+	extends AbstractUIPlugin {
+
+	/**
+	 * The shared plug-in instance
+	 */
+	private static LayoutPlugin plugin;
+ 
+	/**
+	 * The plug-in resource bundle
+	 */
+	private ResourceBundle resourceBundle;
+
+	/**
+	 * Constructor
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#AbstractUIPlugin()
+	 */
+	public LayoutPlugin() {
+		super();
+		plugin = this;
+		try {
+			resourceBundle = ResourceBundle
+				.getBundle("org.eclipse.gmf.examples.runtime.diagram.layout.LayoutPluginResources"); //$NON-NLS-1$
+		} catch (MissingResourceException x) {
+			resourceBundle = null;
+		}
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static LayoutPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the workspace instance.
+	 */
+	public static IWorkspace getWorkspace() {
+		return ResourcesPlugin.getWorkspace();
+	}
+
+	/**
+	 * Returns the string from the plugin's resource bundle,
+	 * or 'key' if not found.
+	 */
+	public static String getResourceString(String key) {
+		ResourceBundle bundle = LayoutPlugin.getDefault().getResourceBundle();
+		try {
+			return (bundle != null ? bundle.getString(key) : key);
+		} catch (MissingResourceException e) {
+			return key;
+		}
+	}
+
+	/**
+	 * Returns the plugin's resource bundle,
+	 */
+	public ResourceBundle getResourceBundle() {
+		return resourceBundle;
+	}
+}
