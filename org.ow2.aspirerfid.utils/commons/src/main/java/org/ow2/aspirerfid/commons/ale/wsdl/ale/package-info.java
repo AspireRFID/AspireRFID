ac9ad8e60e7ae00648490d7ff3fb1b9db6f37625
Nikos Kefalakis <nkef@users.noreply.github.com>
2012-03-22 21:45:13 +0000

    Updated to CXF 2.5.2 auto generated code

---
+++ org.ow2.aspirerfid.utils/commons/src/main/java/org/ow2/aspirerfid/commons/ale/wsdl/ale/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:epcglobal:ale:wsdl:1")
+package org.ow2.aspirerfid.commons.ale.wsdl.ale;
---
+++ org.ow2.aspirerfid.utils/commons/src/main/java/org/ow2/aspirerfid/commons/epcis/model/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:epcglobal:epcis-query:xsd:1")
+package org.ow2.aspirerfid.commons.epcis.model;
